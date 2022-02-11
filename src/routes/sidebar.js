68b6c1701c1f248f8d0727350859ae2ba39d56e0
Petrovic Luka (Consulente) <luka.petrovic@consulenti.sella.it>
2022-02-11 22:17:43 +0100

    setup

---
+++ src/routes/sidebar.js
@@ -0,0 +1,19 @@
+const routes = [
+  {
+    path: '/app/vitals', 
+    icon: 'HomeIcon', 
+    name: 'Vitals',
+  },
+  {
+    path: '/app/e-commerce',
+    icon: 'FormsIcon',
+    name: 'E-commerce',
+  },
+  {
+    path: '/app/marketing',
+    icon: 'CardsIcon',
+    name: 'Marketing',
+  },
+]
+
+export default routes
